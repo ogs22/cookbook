f8bbb2cf3997ee085581848ea91028b5995db8b1
Owen Smith <ogs22@cam.ac.uk>
2014-02-24 16:26:22 +0000

    using pandoc to convert

--- thesaurus-dump.php
+++ thesaurus-dump.php
@@ -1,5 +1,8 @@
 <?php
 
+define("PATH", "/Users/ogs22/gloss/");
+require_once 'markdownify/markdownify.php';
+
 function cleanup($data='') {
     $config = array(
        // 'indent' => false,
@@ -27,25 +30,84 @@ function cleanup($data='') {
     return trim($string);
 }
 
+function md($content) {
+    $md = new Markdownify;
+    $string = $md->parseString(trim($content));
+    return trim($string);
+}
+
+function pdmd($file) {
+//    'pandoc -f html -t markdown hello.html'
+    $cmd = 'pandoc -f html -t markdown \''.PATH.$file.'.html\' -o \''.PATH.$file.'.md\'';
+    exec($cmd);
+}
+
+
+function makeName($title) {
+    $name = trim(strtolower($title));
+    $name = preg_replace("/\\\'/","_",$name);
+    $name = preg_replace("/\'/","_",$name);
+    $name = preg_replace("/\s/","_",$name);
+    $name = str_replace('"',"_",$name);
+    $name = str_replace('*',"_",$name);
+    $name = str_replace('/',"_",$name);
+    $name = str_replace(':',"_",$name);
+    $name = str_replace('<',"_",$name);
+    $name = str_replace('>',"_",$name);
+    $name = str_replace('&',"_",$name);
+    $name = str_replace(';',"_",$name);
+    $name = str_replace('?',"_",$name);
+    $name = str_replace('|',"vbar",$name);
+    $name = str_replace("__","_",$name);
+    return $name;
+}
+
+function checkDupe(&$name,$depth=1) {
+    if (file_exists(PATH.$name.".md")) {
+        $name = $name."I";
+        checkDupe($name,$depth++);
+    }
+}
+
 
 $link = mysqli_connect("localhost","root","tyuhbvcf","thesaurus") or die("Error " . mysqli_error($link));
-$query = 'select name,definition from names
+$query = 'select definitions.concept_no,name,definition from names
 join definitions on definitions.concept_no = names.concept_no 
-where definitions.langcode = "gb" and names.langcode = "gb" ' or die("Error" . mysqli_error($link));
+where definitions.langcode = "gb" and names.langcode = "gb" group by definitions.concept_no' or die("Error" . mysqli_error($link));
 $result = $link->query($query);
 
+echo mysqli_num_rows($result);
 
 
 while($row = mysqli_fetch_assoc($result)) {
   
   	$def = utf8_encode($row['definition']);
-  	$x[utf8_encode($row['name'])] = cleanup($def);
-  
+  	$clean = cleanup($def);
+//    $md = md($clean);
+    $name = makeName($row['name']);
+    checkDupe($name);
+    echo $row['name']."::";
+    echo $row['concept_no']."::";
+    echo $name.".md\n";
+//    file_put_contents(PATH.$name.".md", $md);
+    file_put_contents(PATH.$name.".html", $clean);
+    pdmd($name);
 
 } 
 
-$yaml = yaml_emit($x);
+//$yaml = yaml_emit($x);
 
-echo $yaml;
+//echo $yaml;
 
-?>
+
+
+
+
+
+
+
+
+
+
+
+
